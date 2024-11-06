cb2f0aea05b3262b8badbbd5ec7f49bf408f571f
CriCrin03 <chris012203@gmail.com>
2024-11-05 20:23:09 -0700

    creacion de paquete con maven battleShip

---
+++ BattleShip/src/main/java/Dominio/Orientation.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author Chris
+ */
+public enum Orientation {
+    
+}
---
+++ BattleShip/src/main/java/com/mycompany/battleship/BattleShip.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.battleship;
+
+/**
+ *
+ * @author Chris
+ */
+public class BattleShip {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
