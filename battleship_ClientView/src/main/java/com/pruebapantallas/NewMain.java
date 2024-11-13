d35315a779786590fee4bcbfc2120beb9274cd15
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2024-11-12 19:45:48 -0700

    AAAA

---
+++ battleship_ClientView/src/main/java/com/pruebapantallas/NewMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.pruebapantallas;
+
+/**
+ *
+ * @author jesus
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        BattleshipGame.main(args);
+    }
+    
+}
