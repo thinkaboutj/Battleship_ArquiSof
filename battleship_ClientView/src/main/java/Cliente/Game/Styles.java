0432346980b4108ba82cbcfd4e799f85331459c1
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2024-12-05 15:37:43 -0700

    Validacion Barcos Co-authored-by: Jesus <jesus_G_1710@hotmail.com> Co-authored-by: Cris <chris012203@gmail.com> Co-authored-by: Angel <angel.santos205750@potros.itson.edu.mx> Co-authored-by: Miguel <migueloz60@outlook.com>

---
+++ battleship_ClientView/src/main/java/Cliente/Game/Styles.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cliente.Game;
+
+/**
+ *
+ * @author jesus
+ */
+public class Styles {
+    // Colores de estilo para las casillas
+    public static final String HOVER_TILE_COLOR = "-fx-background-color: rgba(0, 128, 255, 0.5);"; // Azul semitransparente
+    public static final String DEFAULT_TILE_COLOR = "-fx-background-color: transparent;"; // Fondo transparente
+    public static final String SHIP_PLACED_COLOR = "-fx-background-color: green;"; // Color para casillas con barco
+
+    // Otros estilos o colores que puedas necesitar
+}
